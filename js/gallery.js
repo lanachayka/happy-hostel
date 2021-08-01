eea452fdfc682fed8eca31a3ab0b2dc190fc9c38
Руслана Підсадюк <ruslana.chaikivska@gail.com>
2021-08-01 17:48:05 +0300

    Gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,34 +1,10 @@
 function addActiveClass(number) {
-  if (
-    document.getElementById("gallery-carousel-1").classList.contains("active")
-  ) {
-    document.getElementById("gallery-carousel-1").classList.remove("active");
-  }
-  if (
-    document.getElementById("gallery-carousel-2").classList.contains("active")
-  ) {
-    document.getElementById("gallery-carousel-2").classList.remove("active");
-  }
-  if (
-    document.getElementById("gallery-carousel-3").classList.contains("active")
-  ) {
-    document.getElementById("gallery-carousel-3").classList.remove("active");
-  }
-  if (
-    document.getElementById("gallery-carousel-4").classList.contains("active")
-  ) {
-    document.getElementById("gallery-carousel-4").classList.remove("active");
-  }
-  if (
-    document.getElementById("gallery-carousel-5").classList.contains("active")
-  ) {
-    document.getElementById("gallery-carousel-5").classList.remove("active");
-  }
-  if (
-    document.getElementById("gallery-carousel-6").classList.contains("active")
-  ) {
-    document.getElementById("gallery-carousel-6").classList.remove("active");
-  }
+  document.getElementById("gallery-carousel-1").classList.remove("active");
+  document.getElementById("gallery-carousel-2").classList.remove("active");
+  document.getElementById("gallery-carousel-3").classList.remove("active");
+  document.getElementById("gallery-carousel-4").classList.remove("active");
+  document.getElementById("gallery-carousel-5").classList.remove("active");
+  document.getElementById("gallery-carousel-6").classList.remove("active");
   switch (number) {
     case 1:
       document.getElementById("gallery-carousel-1").classList.add("active");
@@ -51,9 +27,33 @@ function addActiveClass(number) {
   }
 }
 
-document.getElementById("1").addEventListener("click", addActiveClass(1));
-document.getElementById("2").addEventListener("click", addActiveClass(2));
-document.getElementById("3").addEventListener("click", addActiveClass(3));
-document.getElementById("4").addEventListener("click", addActiveClass(4));
-document.getElementById("5").addEventListener("click", addActiveClass(5));
-document.getElementById("6").addEventListener("click", addActiveClass(6));
+document.getElementById("1").addEventListener("click", addActiveClass1);
+document.getElementById("2").addEventListener("click", addActiveClass2);
+document.getElementById("3").addEventListener("click", addActiveClass3);
+document.getElementById("4").addEventListener("click", addActiveClass4);
+document.getElementById("5").addEventListener("click", addActiveClass5);
+document.getElementById("6").addEventListener("click", addActiveClass6);
+
+function addActiveClass1() {
+  addActiveClass(1);
+}
+
+function addActiveClass2() {
+  addActiveClass(2);
+}
+
+function addActiveClass3() {
+  addActiveClass(3);
+}
+
+function addActiveClass4() {
+  addActiveClass(4);
+}
+
+function addActiveClass5() {
+  addActiveClass(5);
+}
+
+function addActiveClass6() {
+  addActiveClass(6);
+}
